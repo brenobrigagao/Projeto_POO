c4a1a27cd90712968910ab3985de09fe6888ed17
Breno Brigagão <brenobrigagaodev@gmail.com>
2025-05-13 13:56:39 -0300

    rota para add no carrinho

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -4,6 +4,8 @@ using FFCE.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using FFCE.DTOs;
+using FFCE.Models;
 
 namespace FFCE.Controllers
 {
@@ -37,5 +39,42 @@ namespace FFCE.Controllers
 
             return Ok(produtos);
         }
+        [HttpPost("adicionar-carrinho")]
+    public async Task<IActionResult> AdicionarAoCarrinho(AddCarrinhoDTO dto)
+    {
+        var usuarioId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+
+        var cliente = await _context.Clientes
+        .Include(c => c.Carrinho)
+        .FirstOrDefaultAsync(c => c.UsuarioId == usuarioId);
+        
+        var produto = await _context.Produtos.FindAsync(dto.ProdutoId);
+        
+        if(produto == null) return NotFound("Produto não encontrado");
+        
+        if(dto.Quantidade <= 0) return BadRequest("Quantidade inválida");
+        
+        var itemExistente = await _context.ItensCarrinho
+        .FirstOrDefaultAsync(i => i.CarrinhoId == cliente.Carrinho.Id && i.ProdutoId == dto.ProdutoId);
+        
+        if(itemExistente != null)
+        {
+            itemExistente.Quantidade += dto.Quantidade;
+        }
+        else
+        {
+            var novoItem = new ItemCarrinho {
+                CarrinhoId = cliente.Carrinho.Id,
+                ProdutoId = dto.ProdutoId,
+                Quantidade = dto.Quantidade
+            };
+            _context.ItensCarrinho.Add(novoItem);
+        }
+
+        await _context.SaveChangesAsync();
+        return Ok("Produto adicionado ao carrinho");
     }
+    }
+
+    
 }
